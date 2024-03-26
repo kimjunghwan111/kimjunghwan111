171b6273a6cc314b74d87f29e480a6d71863b55e
kimjunghwan111 <163234790+kimjunghwan111@users.noreply.github.com>
2024-03-26 21:17:04 +0900

    Add files via upload

---
+++ 21100210 자바3주차 실습/b1.java
@@ -0,0 +1,16 @@
+package b;
+
+public class b1 {
+public static void main(String[] args) {
+	int row =2;
+	while (row <10) {
+		int column =1;
+		while(column < 10) {
+			System.out.printf("%4d",row*column);
+			column++;
+		}
+		System.out.println();
+		row++;
+	}
+}
+}
---
+++ 21100210 자바3주차 실습/b3.java
@@ -0,0 +1,13 @@
+package b;
+
+public class b3 {
+public static void main(String[] args) {
+	int i = 1, j = 5;
+	
+	while (true) {
+		System.out.print(i++);
+		if (i>+j)
+			break;
+	}
+}
+}
